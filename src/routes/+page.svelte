4ed253c68514b4b6325eaabcc6cfdbe4fda8b307
Denali Schlesinger <89662251+dschlesinger@users.noreply.github.com>
2025-09-18 17:10:59 +0000

    Add notes on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,4 +8,13 @@
 
 </script>
 
-<KekuleViewer bind:chemicalName bind:getSMILES bind:setSMILES />
+<div class="flex flex-col gap-y-0.5">
+    <h2>Hello this is a site to practice UIPAC naming conventions for BU CAS CH 203</h2>
+    <div></div>
+    <div class='block'>- <a class='text-blue-500' href='/playground'>Playground</a> is a sandbox where you draw and get a UIPAC name</div>
+    <div class='block'>- <a class='text-blue-500' href='/test/name'>Naming Test</a> is a test where you are given a structure and give a UIPAC name</div>
+    <div class='block'>- <a class='text-blue-500' href='/test/draw'>Drawing Test</a> is a test where you are given a UIPAC name and give a structure, it uses cannonical smiles repersentations (I believe) for checking so don't worry about orientation</div>
+    <div></div>
+    <h4>- In Naming & Drawing test the side bar has previous examples, if you click on an example it will bring up a card with more information on the example for review</h4>
+    <h4>- Previous examples will not persist after a refresh!</h4>
+</div>
