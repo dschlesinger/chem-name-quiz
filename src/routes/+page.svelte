df9118b37bd31a1bdf01792d6b9e29a7ae565ff2
Denali Schlesinger <89662251+dschlesinger@users.noreply.github.com>
2025-09-18 17:11:22 +0000

    Remove legacy home script

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,4 @@
 <script>
-    import Button from "$lib/components/ui/button/button.svelte";
-    import KekuleViewer from '$lib/components/custom/kekuleContainer.svelte';
-
-    let chemicalName = $state({current: ''});
-    let getSMILES = $state({current: null})
-    let setSMILES = $state({current: null})
 
 </script>
 
